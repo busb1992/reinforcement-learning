39c9436a4eb7bb2b4613ade3cdefa13fa8a2c537
bbus <balint.bus@nokia.com>
2021-01-06 09:08:01 +0100

    environment.py - finalised environemnt workflow

--- train/tic_tac_toe/environemnt.py
+++ train/tic_tac_toe/environemnt.py
@@ -1,7 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
+x_marker = 'x'
+o_marker = 'o'
+nothing_marker = '-'
+
 
 class MarkBuilder:
+
     def __init__(self, ax, state_obj):
         self.axes = ax
         self.state = state_obj
@@ -29,7 +36,7 @@ class MarkBuilder:
         else:
             return 66
 
-class drawing_environment:
+class environment:
 
     def __init__(self, player1 = 'human', player2 = 'human'):
         self.state = state_of_env()
@@ -46,6 +53,9 @@ class drawing_environment:
 
         self.__draw_env__()
         
+        self.marker_hash_value_X = 1
+        self.marker_hash_value_O = 2
+        
     def __draw_env__(self):
         fig = plt.figure()
         self.ax = fig.add_subplot(111)
@@ -61,29 +71,39 @@ class drawing_environment:
         
         plt.show()
         
-    def __convert_2_xy__(self, moove):
-        convert_table = {0 : [1, 1],
-                         1 : [1, 2],
-                         2 : [1, 3],
-                         3 : [2, 1],
-                         4 : [2, 2],
-                         5 : [2, 3],
-                         6 : [3, 1],
-                         7 : [3, 2],
-                         8 : [3, 3]}
-        return convert_table[moove][1], convert_table[moove][0]
-        
     def new_point(self, moove):
-        pos_x, pos_y = self.__convert_2_xy__(moove)
+        pos_x, pos_y = self.state.__convert_2_xy__(moove)
         marker = self.state.get_marker()
         self.ax.plot(pos_x - 0.5, pos_y - 0.5,
                 marker, markersize=30, color='black',
                 linewidth=10,  mfc='none')
         self.state.set_env(pos_x, pos_y, marker)
+    
+    def set_env_pont(self, positnion_row, positnion_col, mark):
+        self.state.set_env(positnion_row, positnion_col, mark)
         
     def get_env_status(self):
         return self.state.get_env()
     
+    def is_game_over(self):
+        return self.state.check_game_over()
+    
+    def get_env_status_as_hash(self):
+        hash_code = 0
+        vectorised_repr = self.get_env_status()
+        for position in range(len(vectorised_repr)):
+            value = 0
+            if (vectorised_repr[position] == o_marker or
+                vectorised_repr[position] == self.marker_hash_value_O):
+                value = self.marker_hash_value_O
+            elif (vectorised_repr[position] == x_marker or
+                  vectorised_repr[position] == self.marker_hash_value_X):
+                value = self.marker_hash_value_X
+
+            hash_code += (3**position) * value
+
+        return hash_code
+
     def get_symbol(self):
         return self.state.get_marker()
     
@@ -94,8 +114,12 @@ class drawing_environment:
 class state_of_env:
     
     def __init__(self):
-        self.environment_list = ['-'] * 9
+        self.environment_list = [nothing_marker] * 9
         self.alternate = True
+        
+        self.nothing_repr_num = 0
+        self.marker_hash_value_X = 1
+        self.marker_hash_value_O = 2
 
         
     def set_env(self, positnion_row, positnion_col, mark):
@@ -103,6 +127,23 @@ class state_of_env:
         
     def get_env(self):
         return self.environment_list
+    
+    def get_env_status_as_hash(self, vectorised_repr_of_state=[]):
+        if len(vectorised_repr_of_state) == 0:
+            vectorised_repr_of_state = self.environment_list
+        hash_code = 0
+        for position in range(len(vectorised_repr_of_state)):
+            value = 0
+            if (vectorised_repr_of_state[position] == o_marker or
+                vectorised_repr_of_state[position] == self.marker_hash_value_O):
+                value = self.marker_hash_value_O
+            elif(vectorised_repr_of_state[position] == x_marker or
+                  vectorised_repr_of_state[position] == self.marker_hash_value_X):
+                value = self.marker_hash_value_X
+                
+            hash_code += (3**position) * value
+
+        return hash_code
     
     def __convert_2_pos__(self, positnion_col, positnion_row):
         if(positnion_row  == 0):
@@ -114,14 +155,83 @@ class state_of_env:
             
         return default_list[positnion_col]
     
+    def __convert_2_xy__(self, pos):
+        convert_table = {0 : [1, 1],
+                         1 : [1, 2],
+                         2 : [1, 3],
+                         3 : [2, 1],
+                         4 : [2, 2],
+                         5 : [2, 3],
+                         6 : [3, 1],
+                         7 : [3, 2],
+                         8 : [3, 3]}
+        return convert_table[pos][1], convert_table[pos][0]
+    
     def get_marker(self):
         if self.alternate:
             self.alternate = False
-            return 'x'
+            return x_marker
         else:
             self.alternate = True
-            return 'o'
-  
-'''
-huhu = drawing_environment()  
-'''
+            return o_marker
+
+    def check_game_ended(self, env_vec=[]):
+        if len(env_vec) == 0:
+            env_vec = self.environment_list
+    
+        if nothing_marker in env_vec or self.nothing_repr_num in env_vec:
+            return False
+        else:
+            return True
+
+    def check_winner(self, env_vec=[]):
+        if len(env_vec) == 0:
+            env_vec = self.environment_list
+            
+        game_finished = False
+
+        # this is not that nice but can be effective
+        winning_positions = [[0,1,2],
+                             [3,4,5],
+                             [6,7,8],
+                             [0,3,6],
+                             [1,4,7],
+                             [2,5,8],
+                             [0,4,8],
+                             [2,4,6]]
+       
+        for current_win_pos in winning_positions:
+            temp_list = [env_vec[i] for i in current_win_pos]
+            if len(set(temp_list))==1:
+                if (temp_list[0] != nothing_marker and
+                    temp_list[0] != self.nothing_repr_num):
+                    return temp_list[0]
+        return game_finished
+
+    def get_states_hash_and_winner(self, curr_position=0, curr_vec=[]):
+        results = []
+        if curr_position == len(curr_vec):
+            return curr_vec[:]
+        else:
+            for value in range(3):
+                curr_vec[curr_position] = value
+                next_vec = self.get_states_hash_and_winner(curr_position+1, curr_vec)[:]
+                if any(isinstance(el, list) for el in next_vec):
+                    results.extend(next_vec)
+                else:
+                    results.append([self.get_env_status_as_hash(next_vec),
+                                    self.check_game_ended(next_vec),
+                                    self.check_winner(next_vec)])
+                    
+    
+        return results
+    
+
+
+state = state_of_env()
+
+
+state.get_env_status_as_hash([1,0,1])
+huhu = state.get_states_hash_and_winner(0,['-','-','-','-','-','-','-','-','-'])
+
+        
